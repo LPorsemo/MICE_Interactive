94a192065f7d6a6a5a621c7d511db46b2b47b9aa
LPorsemo <Linus.porsemo@gmail.com>
2015-05-15 10:38:04 +0200

    Clean slate

---
+++ src/mice/interactive/MICEInteractive.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mice.interactive;
+import oru.inf.InfDB;
+import oru.inf.InfException;
+import javax.swing.JOptionPane;
+        
+
+/**
+ *
+ * @author Jakob
+ */
+public class MICEInteractive {
+   
+    private InfDB databas;
+    
+     
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
